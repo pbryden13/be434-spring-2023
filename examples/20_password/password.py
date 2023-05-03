df41659429db1b4083382a896680850c797b2b83
pbryden13 <122506714+pbryden13@users.noreply.github.com>
2023-05-02 22:52:08 -0700

    fix

--- examples/20_password/password.py
+++ examples/20_password/password.py
@@ -2,10 +2,13 @@
 """
 Author : philipbryden <philipbryden@localhost>
 Date   : 2023-04-07
-Purpose: password example code
+Purpose: create password example code
 """
 
 import argparse
+import random
+import re
+import string
 
 
 # --------------------------------------------------
@@ -16,34 +19,50 @@ def get_args():
         description='password ex code',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
+    parser.add_argument('file',
+                        help='Input file(s)',
                         metavar='FILE',
-                        type=argparse.FileType('rt'),
+                        nargs='+',
+                        type=argparse.FileType('rt'))
+
+    parser.add_argument('-n',
+                        '--num',
+                        help='Number of passwords to generate',
+                        metavar='num',
+                        type=int,
+                        default='3')
+
+    parser.add_argument('-w',
+                        '--num_words',
+                        help='Number of words per password',
+                        metavar='words',
+                        type=int,
+                        default=4)
+
+    parser.add_argument('-m',
+                        '--min_word_len',
+                        help='Minium length of word',
+                        metavar='words',
+                        type=int,
+                        default=3)
+
+    parser.add_argument('-x',
+                        '--max_word_len',
+                        help='Maxinum length of word',
+                        metavar='max',
+                        type=int,
+                        default=6)
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='',
+                        metavar='Seed for random',
+                        type=int,
                         default=None)
 
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
+    parser.add_argument('-l',
+                        '--l33t',
+                        help='Obfuscate the password',
                         action='store_true')
 
     return parser.parse_args()
@@ -51,22 +70,68 @@ def get_args():
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Make password prog work here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    random.seed(args.seed)
+    words = set()
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    def word_len(word):
+        return args.min_word_len <= len(word) <= args.max_word_len
+
+    for fh in args.file:
+        for line in fh:
+            for word in filter(word_len, map(clean, line.lower().split())):
+                words.add(word.title())
+
+    words = sorted(words)
+
+    passwords = []
+    for _ in range(args.num):
+        passwords.append(''.join(random.sample(words, k=args.num_words)))
+        #print(l33t(password) if args.l33t else password)
+
+    if args.l33t:
+        #passwords = [l33t(word) for word in passwords]
+        passwords = map(l33t, passwords)
+
+    print('\n'.join(passwords))
 
 
 # --------------------------------------------------
+def clean(word):
+    """Remove non-letters from word"""
+
+    return re.sub('[^a-zA-Z]', '', word)
+
+
+# --------------------------------------------------
+def ransom(text):
+    """Randomly choose an upper or lowercase letter to return"""
+
+    return ''.join(
+        map(lambda c: c.upper() if random.choice([0, 1]) else c.lower(), text))
+
+
+# -------------------------------------------------
+def l33t(text):
+    """l33t"""
+
+    text = ransom(text)
+    xform = str.maketrans({
+        'a': '@',
+        'A': '4',
+        'O': '0',
+        't': '+',
+        'E': '3',
+        'I': '1',
+        'S': '5'
+    })
+
+    return text.translate(xform) + random.choice(string.punctuation)
+
+
+# -------------------------------------------------
+
 if __name__ == '__main__':
     main()
